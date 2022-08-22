88bd0ab853e8d31e434c4b80884a1eb5e6711b69
KayAhn0126 <dkseotn123@gmail.com>
2022-08-22 23:16:42 +0900

    refactor: collectionView dataSource와 layout 구현

--- InstaSearch/InstaSearch/SearchCollectionViewCell.swift
+++ InstaSearch/InstaSearch/SearchCollectionViewCell.swift
@@ -9,4 +9,14 @@ import UIKit
 
 class SearchCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    
+    override func prepareForReuse() { // UICollectionViewCell 안에있는 메서드
+        super.prepareForReuse() // subclass에서는 왜 super클래스의 메서드를 호출해야 할까? 자세히 알아보자!
+        thumbnailImageView.image = nil
+    }
+    
+    func configure(_ imageName: String) {
+        thumbnailImageView.image = UIImage(named: imageName)
+    }
 }
--- InstaSearch/InstaSearch/SearchViewController.swift
+++ InstaSearch/InstaSearch/SearchViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        if let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            flowLayout.estimatedItemSize = .zero
+        }
     }
+}
 
+extension SearchViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 24
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SearchCollectionViewCell", for: indexPath) as? SearchCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        let imageName = "animal\(indexPath.item + 1)"
+        cell.configure(imageName)
+        return cell
+    }
+}
 
+extension SearchViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let interItemSpacing: CGFloat = 1
+        let width = (collectionView.bounds.width - interItemSpacing * 2) / 3
+        let height = width
+        return CGSize(width: width, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
 }
 
