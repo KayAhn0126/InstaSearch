25af16af1ea72aa6a34e6fde0ee38b02922c1058
KayAhn0126 <dkseotn123@gmail.com>
2022-08-22 22:45:56 +0900

    refactor: collectionView 생성

---
+++ InstaSearch/InstaSearch/SearchCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  SearchCollectionViewCell.swift
+//  InstaSearch
+//
+//  Created by Kay on 2022/08/22.
+//
+
+import UIKit
+
+class SearchCollectionViewCell: UICollectionViewCell {
+    
+}
--- InstaSearch/InstaSearch/SearchViewController.swift
+++ InstaSearch/InstaSearch/SearchViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
